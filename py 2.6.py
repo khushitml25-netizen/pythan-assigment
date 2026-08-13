2089e1f5a01d4b72fcfa08228572710d25544798
Khushi <khushit.ml25@sbjit.edu.in>
2026-08-13 23:16:24 +0530

    sixth of second

---
+++ py 2.6.py
@@ -0,0 +1,10 @@
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("orange")
+print("After adding:", fruits)
+
+fruits.remove("banana")
+print("After removing:", fruits)
+
+fruits.discard("mango")
+print("After discarding:", fruits)
